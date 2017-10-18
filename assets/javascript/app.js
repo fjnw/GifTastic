b8dfaf730ea39e58358c8f8cc914f35576793a77
fjnw <faizanjamal2018@u.northwestern.edu>
2017-10-18 18:08:47 -0500

    Background changed. Gifs now render in Bootstrap cards. CSS completed.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,7 +6,7 @@ function renderButtons(){
 	$("#btn-area").empty();
 	for (var i=0; i<tags.length; i++) {
 		var btn = $("<button>");
-		btn.addClass("btn btn-info gif-btn");
+		btn.addClass("btn btn-outline-info gif-btn");
 		btn.attr("data-name", tags[i]);
 		btn.attr("offset", 0);
 		btn.text(tags[i]);
@@ -52,8 +52,7 @@ $(document).on("click", ".gif-btn", function(){
 	$("#image-area").empty();
 	// set button's data-namme to variable for searching
 	var gif = $(this).attr("data-name");
-	//var offset = $(this).attr("offset");
-	// API url, with data-name as the search value, limited to 10 results, rated G, w/ public API key
+	// API url, with data-name as the search value, limited quanitity, rated G, w/ public API key
 	// API documents: https://developers.giphy.com/docs/
 	var queryUrl = "https://api.giphy.com/v1/gifs/search?q=" + gif + 
 		"&limit=" + limit + 
@@ -67,25 +66,35 @@ $(document).on("click", ".gif-btn", function(){
    	// build div and display image/text for each Gif in JSON response 
    	for (i = 0; i < response.pagination.count; i++) {
    		// create Div container for each gif and related elements
-	   	var gifDiv = $('<div class="gif-results">');
+	   	var cardDiv = $('<div class="card text-center border-info mb-3" style="width: 20rem;">');
 	   	// create img, set all needed Gif attributes to this img, then append to new Div 
-	   	$('<img>').attr({ // http://api.jquery.com/jQuery/#jQuery2   --"Creating New Elements"
+	   	// shorthand: http://api.jquery.com/jQuery/#jQuery2  --"Creating New Elements"
+	   	$('<img class="card-img-top">').attr({ 
 	   		"src": response.data[i].images.fixed_width_still.url ,
 	   		"data-still": response.data[i].images.fixed_width_still.url ,
 	   		"data-animate": response.data[i].images.fixed_width.url ,
 	   		"data-state": "still",
 	   		"class": "gif",
 	   		"alt": response.data[i].title
-	   	 }).appendTo(gifDiv);
-	   	// get additional info values from JSON and displays in html
-	   	$(	'<p>TITLE: ' + response.data[i].title + 
-	   		'<br>RATING: ' + response.data[i].rating.toUpperCase() + '</p>'
-	   	).appendTo(gifDiv);
+	   	 }).appendTo(cardDiv);
+	   	// Gif img, additional info, and button link in Bootstrap card
+	   	var cardBody = $('<div class="card-body">');
+	   	$(	'<h4 class="card-title">' + response.data[i].title + '</h4>' +
+	   		'<p class="card-text"><small class="text-muted">rated: ' + 
+	   		response.data[i].rating.toUpperCase() + 
+	   		'<small></p>'
+	   	).appendTo(cardBody);
+	   	// adds button with link to card
+	   	$( '<a href=' + response.data[i].images.original.url + 
+	   		' class="btn btn-info">View Original</a>').appendTo(cardBody);
+	   	// appends cardBody to cardDiv
+	   	cardDiv.append(cardBody);
 	   	// add new div to image area
-	   	$("#image-area").append(gifDiv);
+	   	$("#image-area").append(cardDiv);
    	}
    });
-  	// increases offset value in gif-btn by display limit to prevent repeat gifs for that search term
+  	// increases offset value in gif-btn by the display limit.
+  	// prevents repeat gifs for that search term
   	$(this).attr( "offset", (parseInt($(this).attr("offset")) + limit) );
 });
 
@@ -112,17 +121,9 @@ $(document).on("click", ".gif", function(){
 	}
 });
 
-
-
-	// var searchTerm = '';
-	// var quantity = 1;
-	// var rating = "g";
-	// var apiKey = "dc6zaTOxFJmzC";
-	// var queryUrl = "http://api.giphy.com/v1/gifs/search?q=" +
-	// 	searchTerm + 
-	// 	"&limit=" +
-	// 	quantity +
-	// 	"&rating=" +
-	// 	rating + 
-	// 	"&api_key=" +
-	// 	apiKey;
+// makes links <a href> open in new tab vs. defaul in same window
+$(document).on('click', 'a', function(e){ 
+    e.preventDefault(); 
+    var url = $(this).attr('href'); 
+    window.open(url, '_blank');
+});
